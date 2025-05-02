25f480f04d02de711ea196ae49234867fdb3af01
Bowie <marketing#letscode.com.br>
2025-05-02 11:38:56 -0300

    feat: Finalize build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/furia-fan-site-mvp/', // Substitua pelo nome exato do seu repositório no GitHub
+    base: '/furia-fan-site-mvp/',
 })
